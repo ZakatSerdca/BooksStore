443f7e3478f5fb54a24f9f502660606a9f7029a6
Daniil <danilcursin@mail.ru>
2023-03-15 17:28:02 +0300

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from store.models import Book
+from store.serializer import BooksSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
