96c134c5020cb5b5d7a4db80dc0dc1157ddc5755
Daniil <danilcursin@mail.ru>
2023-03-17 19:47:14 +0300

    0.01(отношение книг и юзеров по лайкам и тд)

--- store/models.py
+++ store/models.py
@@ -6,8 +6,11 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
+    # если мигрировать без related_name при двух User, то комп хз,
+    # какие ему учитывать выборки, овнеров или редэрсов, поэтому делаем
     owner = models.ForeignKey(User, on_delete=models.SET_NULL,
-                              null=True)
+                              null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')  # тут привязка к тому классу внизу
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
