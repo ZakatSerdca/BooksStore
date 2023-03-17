4f6fa203464d91ff7329bd679b96b2b70d40309c
Daniil <danilcursin@mail.ru>
2023-03-17 19:34:39 +0300

    add likes, bookmarks and rate

--- store/models.py
+++ store/models.py
@@ -13,5 +13,19 @@ class Book(models.Model):
         return f'Id {self.id}: {self.name}'
 
 
-    def a(self):
-        pass
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (1, 'Fine'),
+        (1, 'Good'),
+        (1, 'Amazing'),
+        (1, 'Incredible')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.book.name}, {self.rate}'
