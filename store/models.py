e82de7c17077b0129539e76720cb09e3ccc21ef3
Daniil <danilcursin@mail.ru>
2023-03-17 20:34:06 +0300

    0.02(Исправлена ошибка значений рейтинга тесты на лайки и рейтинг)

--- store/models.py
+++ store/models.py
@@ -19,16 +19,16 @@ class Book(models.Model):
 class UserBookRelation(models.Model):
     RATE_CHOICES = (
         (1, 'Ok'),
-        (1, 'Fine'),
-        (1, 'Good'),
-        (1, 'Amazing'),
-        (1, 'Incredible')
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible')
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     like = models.BooleanField(default=False)
     in_bookmarks = models.BooleanField(default=False)
-    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
 
     def __str__(self):
         return f'{self.user.username}: {self.book.name}, {self.rate}'
