a10eaf04697e11d14d00b39efb9a71c80c6e912a
Daniil <danilcursin@mail.ru>
2023-03-28 20:41:32 +0300

    0.023(???)

--- store/serializer.py
+++ store/serializer.py
@@ -5,18 +5,17 @@ from store.models import Book, UserBookRelation
 
 
 class BooksSerializer(ModelSerializer):
-    likes_count = serializers.SerializerMethodField()
+    annotated_likes = serializers.IntegerField(read_only=True)
+    rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
+    owner_name = serializers.CharField(source='owner.username', default='', read_only=True)
 
     class Meta:
         model = Book
-        fields = ('id', 'name', 'price', 'author_name', 'likes_count')
-
-    def get_likes_count(self, instance):
-        return UserBookRelation.objects.filter(book=instance, like=True).count()
+        fields = ('id', 'name', 'price', 'author_name', 'annotated_likes',
+                  'rating', 'owner_name',)
 
 
 class UserBookRelationSerializer(ModelSerializer):
     class Meta:
         model = UserBookRelation
         fields = ('book', 'like', 'in_bookmarks', 'rate')
-
--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Case, When, Avg
 from django.test import TestCase
 
 from django.contrib.auth.models import User
@@ -14,31 +15,38 @@ class BookSerializerTestCase(TestCase):
         book_1 = Book.objects.create(name='Test Book 1', price=25, author_name='Author 1')
         book_2 = Book.objects.create(name='Test Book 2', price=55, author_name='Author 2')
 
-        UserBookRelation.objects.create(user=user_1, book=book_1, like=True)
-        UserBookRelation.objects.create(user=user_2, book=book_1, like=True)
-        UserBookRelation.objects.create(user=user_3, book=book_1, like=True)
+        UserBookRelation.objects.create(user=user_1, book=book_1, like=True, rate=5)
+        UserBookRelation.objects.create(user=user_2, book=book_1, like=True, rate=5)
+        UserBookRelation.objects.create(user=user_3, book=book_1, like=True, rate=4)
 
-        UserBookRelation.objects.create(user=user_1, book=book_2, like=True)
-        UserBookRelation.objects.create(user=user_2, book=book_2, like=True)
+        UserBookRelation.objects.create(user=user_1, book=book_2, like=True, rate=3)
+        UserBookRelation.objects.create(user=user_2, book=book_2, like=True, rate=4)
         UserBookRelation.objects.create(user=user_3, book=book_2, like=False)
 
-        data = BooksSerializer([book_1, book_2], many=True).data
+        books = Book.objects.all().annotate(
+            annotated_likes=Count(Case(When(userbookrelation__like=True, then=1))),
+            rating=Avg('userbookrelation__rate')
+        ).order_by('id')
+        data = BooksSerializer(books, many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test Book 1',
                 'price': '25.00',
                 'author_name': 'Author 1',
-                'likes_count': 3
+                'annotated_likes': 3,
+                'rating': '4.67'
             },
             {
                 'id': book_2.id,
                 'name': 'Test Book 2',
                 'price': '55.00',
                 'author_name': 'Author 2',
-                'likes_count': 2
+                'annotated_likes': 2,
+                'rating': '3.50'
             }
         ]
+        # print(data)
         self.assertEqual(expected_data, data)
 
 
